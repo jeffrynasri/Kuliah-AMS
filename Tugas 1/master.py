8f3d762db8bd3effcee0c184033ac68ca1300a3f
jeffrynasri <jeffryfaruki@Gmail.com>
2018-03-03 11:26:59 +0700

    Hitung ce dan cp dan mencari 5 terbaik

--- Tugas 1/master.py
+++ Tugas 1/master.py
@@ -8,33 +8,62 @@ Created on Wed Feb 28 16:06:11 2018
 import snap
 import numpy as np
 from scipy import linalg as la
+from collections import OrderedDict
 
-def calculate_eigen():
-	A=np.array([[5,-3,3],
-			   [4,-2,3],
-			   [4,-4,5]])
+def CalculateEigenVectorCentr(graph):
+	output={}
+	NIdEigenH = snap.TIntFltH()
+	snap.GetEigenVectorCentr(graph, NIdEigenH)
+	
+	for item in NIdEigenH:
+		#print "node: %d have centrality: %f" % (item, NIdEigenH[item])
+		output[item]=NIdEigenH[item]
+	return output
+		
+def CalculatePageRank(graph,alpha,number_iteration):
+	PRankH=snap.TIntFltH()
+	snap.GetPageRank(graph,PRankH,alpha, 1e-4, number_iteration)
+	output={}
+	for item in PRankH:
+		output[item]=PRankH[item]
+	return output
+def transverse_graph(graph):
+	for NI in graph.Nodes():
+		print "node: %d, degree %d" % ( NI.GetId(), NI.GetDeg())
+		for br in NI.GetOutEdges():
+			print br
 
-	e_vals, e_vecs = la.eig(A)
+#------------------ CREATING GRAPH from .edges file--------------------------------------
+file=open("facebook/0.edges","r")
+lines=file.readlines()
+G1 = snap.TUNGraph.New()
+for line in lines:
+	line=line.strip("\n")
+	line=map(int,line.split(" "))
+	if not G1.IsNode(line[0]):
+		G1.AddNode(line[0])
+	if not G1.IsNode(line[1]):
+		G1.AddNode(line[1])
+	if not G1.IsEdge(line[0],line[1]):
+		G1.AddEdge(line[0],line[1])
+#transverse_graph(G2)
+#------------------------------------------------------------------------------------------------
+#------------------ FIND 5 GREATEST CENTRALITY BASED ON EIGENVECTOR-----------
+ce=CalculateEigenVectorCentr(G1)
+#Sorting DSC the ce(Centrality Eigenvector)
+ce=OrderedDict(sorted(ce.items(),key=lambda x:x[1],reverse=True))
+print "5 Greatest Centrality Based On EigenVector"
+for i in ce.keys()[:5]:
+	print "node: %d have eigenvector: %f" % (i, ce[i])
 
-	print(e_vals.astype(float))
-	A=np.array([[0,1,0],
-			   [1,0,1],
-			   [0,1,0]])
 
-	e_vals, e_vecs = la.eig(A)
-
-	print(e_vals)
-
-calculate_eigen()
-# create a graph PNGraph
-G1 = snap.TNGraph.New()
-G1.AddNode(1)
-G1.AddNode(5)
-G1.AddNode(32)
-G1.AddEdge(1,5)
-G1.AddEdge(5,1)
-G1.AddEdge(5,32)
-
-
-
+#------------------------------------------------------------------------------------------------
+#------------------ FIND 5 GREATEST CENTRALITY BASED ON EIGENVECTOR-----------
+cp=CalculatePageRank(G1,1,100)
+#Sorting DSC the ce(Centrality PageRank)
+cp=OrderedDict(sorted(cp.items(),key=lambda x:x[1],reverse=True))
+print "5 Greatest Centrality Based On Page Ranking"
+for i in cp.keys()[:5]:
+	print "node: %d have page rank: %f" % (i, cp[i])
+#-------------------------------------------------------------------------------------------------
 
